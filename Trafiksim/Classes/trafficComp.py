0f3440ad45859fdf61943cebdf35976f05c6730e
Ludwig Palm <ludwigpalm@hotmail.com>
2022-10-04 16:40:35 +0200

    Added Comments

--- Trafiksim/Classes/trafficComp.py
+++ Trafiksim/Classes/trafficComp.py
@@ -3,9 +3,12 @@ class Vehicle:
 
     def __init__(self, destination, borntime):
         """Creates the vehicle with specified properties."""
+        # puts the variables in the class(instance)
         self.destination = destination
         self.borntime = borntime
     def __str__(self):
+        """String representation of a vehicle"""
+        
         return f"Vehicle({self.destination}, {self.borntime})"
 
 class Lane:
@@ -13,9 +16,11 @@ class Lane:
 
     def __init__(self, length):
         """Creates a lane of specified length."""
+        # Creates a list filled with None for the length of the lane
         self.lane = [None for i in range(length)]
     def __str__(self):
         """String representation of lane contents."""
+        # Makes a string representation of correct format
         returnString = "["
         for val in self.lane:
             returnString += f"{val.destination if val != None else str('.') } "
@@ -23,13 +28,16 @@ class Lane:
 
     def enter(self, vehicle):
         """Called when a new vehicle enters the end of the lane."""
+        # Places that vehicle at the end of the list
         self.lane[-1] = vehicle
     def last_free(self):
         """Reports whether there is space for a vehicle at the
         end of the lane."""
+        # Checks of the last place of the list is None or empty
         return self.lane[-1] == None
     def step(self):
         """Execute one time step."""
+        # Loops through the lane and moves every vehicle forward if there is space and vehicle is not on the first spot(index 0)
         for index ,val  in enumerate(self.lane):
             if self.lane[index-1] is None and index-1 >= 0:
                 self.lane[index-1] = self.lane[index]
@@ -37,25 +45,29 @@ class Lane:
             
     def get_first(self):
         """Return the first vehicle in the lane, or None."""
-        for x in self.lane:
-            if x is not None:
-                return x
+        # If lane at index 0(first place) is a vehicle, return that vehicle
+        
+        if self.lane[0] is not None:
+            return self.lane[0]
     def remove_first(self):
         """Remove the first vehicle in the lane.
            Return the vehicle removed.
            If no vehicle is a the front of the lane, returns None
            without removing anything."""
-           #not the nicest solution but fuck it
-        returnval = self.lane.pop(0)
-        self.lane.insert(0, None if returnval is not None else returnval)
-        return returnval
+           # not the nicest solution but fuck it
+           # probably dont do this solution, use a loop and an if statement instead
+           # I will explain this anyway tho
+        returnval = self.lane.pop(0) # Pops, or removes the vehicle at the first place(index 0) and assign it to returnval
+        self.lane.insert(0, None if returnval is not None else returnval) # insert back into the list(at index 0) either None, if returnval is a vehicle(aka first spot was a vehicle) or the returnval if it is None(so it will insert none or none, i said it was a bad solution)
+        return returnval # return either a vehicle or none
     def number_in_lane(self):
         """Return the number of vehicles currently in the lane."""
+        # loops through the list and adds 1 to counter everytime it finds a vehicle
         counter = 0
         for x in self.lane:
             if x != None:
                 counter += 1
-        return counter
+        return counter # returns the number of vehicles it found
 
 def demo_lane():
     """For demonstration of the class Lane"""
@@ -82,24 +94,29 @@ def demo_lane():
 
 class Light:
     """Represents a traffic light"""
+    # just counts up 1 for every step
     step_timer = 0
     
 
     def __init__(self, period, green_period):
         """Create a light with the specified timers."""
+        # insert the values into the class(technically the instance), you need to do this to access period and green_preiod in the other functions(methods)
         self.period = period
         self.green_period = green_period
 
     def __str__(self):
         """Report current state of the light."""
+        # prints it with correct format
         return f"( {str('G')  if self.is_green() else str('R')} )"
 
     def step(self):
         """Take one light time step."""
+        # takes one step aka adds 1 to the step_counter
         self.step_timer += 1
 
     def is_green(self):
         """Return whether the light is currently green."""
+        # returns true if stepTimer modulus period is less then the green period(check my math if u dont belive me)
         return True if (self.step_timer) % (self.period) < self.green_period else False
 
 
@@ -121,4 +138,5 @@ def main():
 
 
 if __name__ == '__main__':
+    # a thing you put into a file to make it run only if you are specifically running that file
     main()
--- Trafiksim/Classes/trafficSystem0.py
+++ Trafiksim/Classes/trafficSystem0.py
@@ -12,15 +12,17 @@ class TrafficSystem:
     def __init__(self):
         """Initialize all components of the traffic
         system."""
-        self.time = 0
-        self.lane_right = tc.Lane(5)
-        self.lane_left = tc.Lane(5)
-        self.s1 = tc.Light(10, 8)
-        self.dest = destinations.Destinations()
-        self.queue = []
+        # Creates all the stuff for our traffic sim
+        self.time = 0 # the time, just a number that counts up for every step
+        self.lane_right = tc.Lane(5) # the right lane with a length of 5
+        self.lane_left = tc.Lane(5) # the left lane with a length of 5
+        self.s1 = tc.Light(10, 8) # a stoplight that will sit between the lanes
+        self.dest = destinations.Destinations() # the premade class that makes the cars 
+        self.queue = [] # the queue for cars waiting to enter the fist lane
 
     def snapshot(self):
         """Print a snap shot of the current state of the system."""
+        # uses the __str__ functions of the classes combined to make a very nice output of the whole system
         q = "["
         for x in self.queue:
             q += f"{x.destination}, "
@@ -29,26 +31,28 @@ class TrafficSystem:
 
     def step(self):
         """Take one time step for all components."""
-        self.time += 1
-        self.lane_left.remove_first()
-        self.lane_left.step()
-        if self.s1.is_green() and self.lane_left.last_free():
-            self.lane_left.enter(self.lane_right.remove_first())
-        self.s1.step()
-        self.lane_right.step()
-        vehicle = self.dest.step()
-        if vehicle != None:
-            self.queue.append(Vehicle(vehicle, self.time))
-        if self.lane_right.last_free() and len(self.queue) >= 1:
-            self.lane_right.enter(self.queue.pop(0))
+        self.time += 1 # increses time by 1
+        self.lane_left.remove_first() # removes the car to the left, if there is a car there, that car has now been deleted
+        self.lane_left.step() # makes all the cars on the left lane move 1 step
+        if self.s1.is_green() and self.lane_left.last_free(): # if the traffic light is green and the next slot is not occupied
+            self.lane_left.enter(self.lane_right.remove_first()) # move a car from the right to the left lane, if a car can be moved
+        self.s1.step() # takes 1 step for the traffic light
+        self.lane_right.step() # makes all the cars on the right lane move 1 step 
+        vehicle = self.dest.step() # checks if a new car is created this step, if vehicle == None, no new car is to be created, otherwise if its a destination, a car should be created this step
+        if vehicle != None: # if a car should be created
+            self.queue.append(Vehicle(vehicle, self.time)) # creates a new car with vehicle as destination and self.time as the borntime, and adds it to the queue
+        if self.lane_right.last_free() and len(self.queue) >= 1: # if the lane is free and there is a car in the queue
+            self.lane_right.enter(self.queue.pop(0)) # takes the car on index 0 and puts it in the right lane
             
             
 
     def in_system(self):
         """Return the number of vehicles in the system."""
+        # cars in laft lane + cars in right lane + cars is queue
         return self.lane_left.number_in_lane() + self.lane_right.number_in_lane() + len(self.queue)
 #region
     # def print_statistics(self):
+    #    # Only used in the voluntary assignment
     #     """Print statistics about the run."""
     #     # Implement this method.
 #endregion
